66d308c238f151fa7ec9f70b47f5c0d4f15323c6
amrit12001703 <amrit.12001703@lpu.in>
2021-12-31 17:01:34 +0530

    Updates

---
+++ precache-manifest.e16cb53608b81927cbe6894f10d99557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9e482fc3a0cf4306dad6388fecfe96",
+    "url": "/Reciepe-Finder-App-Main/index.html"
+  },
+  {
+    "revision": "57fd3be6f460d0e45a89",
+    "url": "/Reciepe-Finder-App-Main/static/css/main.881fd997.chunk.css"
+  },
+  {
+    "revision": "d55f8d85b2ddc4a54236",
+    "url": "/Reciepe-Finder-App-Main/static/js/2.ddb34275.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/Reciepe-Finder-App-Main/static/js/2.ddb34275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fd3be6f460d0e45a89",
+    "url": "/Reciepe-Finder-App-Main/static/js/main.02633d86.chunk.js"
+  },
+  {
+    "revision": "73419e88e3b8af21432a",
+    "url": "/Reciepe-Finder-App-Main/static/js/runtime-main.60ebe002.js"
+  }
+]);
